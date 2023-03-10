2c7f7905371fd7bbaa34ba3f6f7ff9bfab374145
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-10 23:02:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const ejs = require("ejs")
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.set("view engine","ejs")
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/wikiDB");
+  }
+
+  const articleSchema = new mongoose.Schema ({
+    title:String,
+    content:String
+  });
+
+  const Article = new mongoose.model ("Article", articleSchema);
+
+
+
+  app.get("/articles", async (req, res) => {
+      await Article.find().then(foundArticles => {
+          res.send(foundArticles);
+      }).catch(err => {
+          console.log(err);
+      })
+  });
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
